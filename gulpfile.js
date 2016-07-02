16d74c5720a4413eee96365f2cc4693774f666c2
Ray Lugo, Jr <rlugojr@gmail.com>
2016-07-02 03:26:29 -0400

    Added validators to gulpfile
    added validators for html, css and js to gulpfile.  tested validators to
    ensure that they function correctly.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,34 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  csslint = require('gulp-csslint'),
+  jshint = require('gulp-jshint'),
+  htmlhint = require('gulp-htmlhint'),
+  del = require('del'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  htmlmin = require('gulp-htmlmin'),
+  cssmin = require('gulp-csso'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  rev = require('gulp-rev');
+
+
+gulp.task('html_check',function() {
+  gulp.src(['/src/*.html','src/views/*.html'])
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter());
+});
+
+gulp.task('css_check', function() {
+  gulp.src(['src/css/*.css','src/views/css/*.css'])
+    .pipe(csslint())
+    .pipe(csslint.reporter('compact'));
+});
+
+gulp.task('js_check', function() {
+  return gulp.src(['src/js/*.js','src/views/js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', { verbose: true }));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
